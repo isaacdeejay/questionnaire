756763056c5d09e79f58bde756034f14fa240b74
elmaw <147252346+isaacdeejay@users.noreply.github.com>
2023-10-16 03:32:04 +0800

    forquestionnare.php

--- forquestionnare.php
+++ forquestionnare.php
@@ -1,5 +1,33 @@
 <?php
-// Retrieve and display answers
+$host = "localhost"; // Database host
+$username = "root"; // Database username
+$password = "humanbehavior.13"; // Database password
+$database = "questionnaire"; // Database name
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process Form Submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $codename = $_POST["codename"];
+    $message = $_POST["message"];
+
+    // Insert the data into the database
+    $sql = "INSERT INTO user_answers (user_name, answer_text) VALUES ('$codename', '$message')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Answer submitted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Retrieve and Display Answers
 $answers = array();
 $result = $conn->query("SELECT * FROM user_answers");
 
@@ -9,14 +37,3 @@ if ($result->num_rows > 0) {
     }
 }
 ?>
-
-<!-- Display answers in your HTML -->
-<div class="answers">
-    <h2>Answers</h2>
-    <?php foreach ($answers as $answer) : ?>
-        <div class="answer">
-            <p><strong>Codename: </strong><?php echo $answer["user_name"]; ?></p>
-            <p><strong>Message: </strong><?php echo $answer["answer_text"]; ?></p>
-        </div>
-    <?php endforeach; ?>
-</div>
